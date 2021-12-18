65e74cc389d334689c2d02d08da42a4e2bc9b690
CodeArtisT75 <pcstar7000@gmail.com>
2021-12-18 10:08:12 +0330

    fix: fix seeder bugs and Simulator model

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -1,49 +1,43 @@
 import mongoose from "mongoose";
+import { DateTime } from "luxon";
 import { Profile } from "../models/Profile";
 import { Simulator } from "../models/Simulator";
 import { Favorite } from "../models/Favorite";
 import { DBURL } from "../config";
 
 (async () => {
-  mongoose.connect(DBURL, {
+  await mongoose.connect(DBURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
   const profile = new Profile({
     name: `String`,
+    nickname: `String`,
     email: `String`,
-    capital: `123`,
+    capital: 123,
     divisa: `String`,
-    prefered_cryptocurrency: `String`,
+    preferredCryptocurrency: `String`,
   });
   await profile.save();
 
-  const query = { _id: "6093abb3dfd9da1deeae56f2" };
-  const idProfile = await Profile.findOne(query).then((e) => {
-    return e?._id;
-  });
-
   const simulator = new Simulator({
-    profile_id: idProfile,
+    profileId: profile.id,
     name: `String`,
-    start_date: `01/05/2021`,
-    check_date: `01/05/2021`,
+    startDate: DateTime.local(2021, 5, 1),
+    checkDate: DateTime.local(2021, 5, 1),
     cryptocurrency: `String`,
-    divisa: `String`,
-    Crypto_price_start: `123`,
-    Crypto_price_check: `123`,
+    cryptoPriceStart: 123,
+    cryptoPriceCheck: 123,
   });
   await simulator.save();
 
   const favorite = new Favorite({
-    profile_id: idProfile,
+    profileId: profile.id,
     name: `String`,
-    favorite1: `String`,
-    favorite2: `String`,
-    favorite3: `String`,
+    favorites: [`String`, `String`, `String`],
   });
   await favorite.save();
 
-  mongoose.disconnect();
+  await mongoose.disconnect();
 })();
