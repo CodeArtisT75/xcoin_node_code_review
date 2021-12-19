75e480baf5d8ef782fa7255c9de7e2c6d423ebf2
CodeArtisT75 <pcstar7000@gmail.com>
2021-12-19 16:47:40 +0330

    fix: make some fields optional in profile validators

--- src/validators/profile.validators.ts
+++ src/validators/profile.validators.ts
@@ -1,11 +1,13 @@
 import { body, query, ValidationChain } from "express-validator";
 
 export const ProfileIndexValidator = (): ValidationChain[] => [
-  query("page", "page must be number").isInt({ min: 1 }),
-  query("perPage", "perPage must be integer and in range 1-100").isInt({
-    min: 1,
-    max: 100,
-  }),
+  query("page", "page must be number").optional().isInt({ min: 1 }),
+  query("perPage", "perPage must be integer and in range 1-100")
+    .optional()
+    .isInt({
+      min: 1,
+      max: 100,
+    }),
 ];
 
 export const ProfileStoreValidator = (): ValidationChain[] => [
@@ -13,5 +15,5 @@ export const ProfileStoreValidator = (): ValidationChain[] => [
     .notEmpty()
     .isEmail(),
   body("name", "nickname is required and must be string").notEmpty().isString(),
-  body("nickname", "name must be string").isString(),
+  body("nickname", "name must be string").optional().isString(),
 ];
